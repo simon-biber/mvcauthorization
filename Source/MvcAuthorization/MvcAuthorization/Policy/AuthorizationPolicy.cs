62395338faff79c540a373cfbe032427bee2399a
RyanMWright_cp <SND\RyanMWright_cp>
2013-05-26 09:14:42 -0700

    Remove SignalR references from MVC project

--- Source/MvcAuthorization/MvcAuthorization/Policy/AuthorizationPolicy.cs
+++ Source/MvcAuthorization/MvcAuthorization/Policy/AuthorizationPolicy.cs
@@ -8,31 +8,11 @@ namespace MvcAuthorization.Policy
     public abstract class AuthorizationPolicy : IAuthorizationPolicy
     {
         /// <summary>
-        /// Globally applies a policy for all request types
+        /// Applies an authorization policy
         /// </summary>
         /// <param name="args"></param>
         /// <returns></returns>
         public virtual ApplyPolicyResult ApplyPolicy(ApplyPolicyArgs args)
-        {
-            return new ApplyPolicyResult() { IsAuthorized = true };
-        }
-
-        /// <summary>
-        /// Applies the policy if the current item is a controller action
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public virtual ApplyPolicyResult ApplyActionPolicy(ApplyActionPolicyArgs args)
-        {
-            return new ApplyPolicyResult() { IsAuthorized = true };
-        }
-
-        /// <summary>
-        /// Applies the policy if the current item is a SignalR hub or PersistentConnection
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public virtual ApplyPolicyResult ApplySignalRPolicy(ApplySignalRPolicyArgs args)
         {
             return new ApplyPolicyResult() { IsAuthorized = true };
         }
--- Source/MvcAuthorization/MvcAuthorization/Policy/IAuthorizationPolicy.cs
+++ Source/MvcAuthorization/MvcAuthorization/Policy/IAuthorizationPolicy.cs
@@ -8,24 +8,10 @@ namespace MvcAuthorization.Policy
     public interface IAuthorizationPolicy
     {
         /// <summary>
-        /// Globally applies a policy for all request types
+        /// Applies an authorization policy
         /// </summary>
         /// <param name="args"></param>
         /// <returns></returns>
         ApplyPolicyResult ApplyPolicy(ApplyPolicyArgs args);
-
-        /// <summary>
-        /// Applies the policy if the current item is a controller action
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        ApplyPolicyResult ApplyActionPolicy(ApplyActionPolicyArgs args);
-
-        /// <summary>
-        /// Applies the policy if the current item is a SignalR hub or PersistentConnection
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        ApplyPolicyResult ApplySignalRPolicy(ApplySignalRPolicyArgs args);
     }
 }
