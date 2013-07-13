be27c65a3a41175e817ae192f40512cfef68468d
RyanMWright_cp <SND\RyanMWright_cp>
2013-07-13 10:07:43 -0700

    Add additional tests

---
+++ Source/MvcAuthorization/MvcAuthorization.Tests/HtmlHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MvcAuthorization.Tests
+{
+    [TestFixture]
+    public class HtmlHelperTests
+    {
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        //[Test]
+        //public void Ensure_Secure_Action_Link()
+        //{
+
+        //}
+    }
+}
---
+++ Source/MvcAuthorization/MvcAuthorization.Tests/PolicyTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MvcAuthorization.Tests
+{
+    [TestFixture]
+    public class PolicyTests
+    {
+        [SetUp]
+        public void Init()
+        {
+        }
+    }
+}
